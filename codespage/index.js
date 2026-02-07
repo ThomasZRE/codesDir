bbf1037f62cef8d6a78e130fec228785c2a45c80
ThomasZRE <zapatat4@gmail.com>
2026-02-07 17:21:19 -0500

    Add query for one account in another service

--- codespage/index.js
+++ codespage/index.js
@@ -38,7 +38,9 @@ app.get('/api/codes/latest', (request, response) => {
     Code.find({
         $or: [
             { subject: { $regex: "Your authentication code", $options: "i" } },
-            { subject: { $regex: "Your ChatGPT code is", $options: "i" } }
+            { subject: { $regex: "Your ChatGPT code is", $options: "i" } },
+            // Getting codes from netflix emails
+            { from: { $regex: "info@account.netflix.com", $options: "i" } }
         ]
     }).sort({ date: -1 }).limit(4).then(codes => {
         response.json(codes)
